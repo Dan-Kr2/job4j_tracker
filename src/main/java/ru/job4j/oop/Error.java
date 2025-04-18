9ff97a7364895d8a0e1b329780289fd5b39cb06d
Dan-Kr2 <x.real.delta@gmail.com>
2025-04-19 00:47:27 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error () {
+
+    }
+
+    public Error (boolean active, int status, String message){
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error one = new Error();
+        Error two = new Error(false, 2,"222");
+        Error three = new Error(true, 3,"333");
+        one.printInfo();
+        two.printInfo();
+        three.printInfo();
+
+    }
+
+}
