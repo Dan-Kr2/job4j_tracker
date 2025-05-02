11cbbbce66ea54f5ce1ba2fc1f78be135dcbcc0a
Dan-Kr2 <x.real.delta@gmail.com>
2025-05-02 15:11:10 +0300

    1.1. Создание объекта [#165452 #540459]

--- src/test/java/ru/job4j/TrackerTest.java
+++ src/test/java/ru/job4j/TrackerTest.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 import org.junit.jupiter.api.Test;
-import ru.job4j.oop.Item;
+import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
 import static org.assertj.core.api.Assertions.assertThat;
