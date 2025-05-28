261d9c502c7c11eb36ec54cb7c2ea31609cb0c61
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-05-28 17:02:13 +0300

    3.1. Массив с пустыми ячейками [#176470]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -6,7 +6,7 @@ public class Shop {
     public static int indexOfNull(Product[] products) {
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
-            if ( product == null) {
+            if (product == null) {
                 return i;
             }
         } return -1;
