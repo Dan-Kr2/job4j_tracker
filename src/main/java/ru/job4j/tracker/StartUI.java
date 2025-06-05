9fcf7e3cf9789e852a6ec3b43c24bf1111f29123
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-06-05 19:40:01 +0300

    3.3. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -38,8 +38,7 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка замены заявки.");
                 }
-        }
-            else if (select == 6) {
+        } else if (select == 6) {
                 run = false;
             }
         }
