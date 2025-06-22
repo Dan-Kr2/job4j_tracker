dcffff7106dc5c57a5399fb26994b6bce86b525d
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-06-22 05:47:57 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,55 +1,6 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.*;
 class StartUITest {
-    @Test
-    void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new MockInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
-
-    @Test
-    void whenCreateItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new MockInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item[] items = tracker.findAll();
-        assertThat(items).hasSize(1);
-        assertThat(items[0].getName()).isEqualTo("Fix PC");
-    }
-
-    @Test
-    void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
-        };
-        StartUI.replaceItem(new MockInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
-    }
-
-    @Test
-    void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-
-        tracker.add(item);
-        int itemId = item.getId();
-        Input mockInput = new MockInput(new String[]{String.valueOf(itemId)});
-        StartUI.deteleItem(mockInput, tracker);
-        Item deletedItem = tracker.findById(itemId);
-        assertThat(deletedItem).isNull();
-    }
+  
 }
