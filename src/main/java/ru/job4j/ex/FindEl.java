485b3f11e7b7b543b1c386660bef9fef461300ac
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-06-25 13:29:03 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,22 +1,26 @@
-/*package ru.job4j.ex;
+package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) {
-        try {
-            int result = -1;
-            int index = 0;
-            for (String str : value) {
-                if (key.equals(str)) {
-                    result = index;
-                    break;
-                }
-                index++;
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int index = 0;
+        for (String str : value) {
+            if (key.equals(str)) {
+                return index;
             }
-            return result;
-        } catch (UserInputException e) {
+            index++;
+        }
+        throw new ElementNotFoundException("Элемент '" + key + "' не найден в массиве");
+    }
 
+    public static void main(String[] args) {
+        String[] data = {"alfa", "beta", "gamma", "delta"};
+        String key = "omega";
+
+        try {
+            int index = indexOf(data, key);
+            System.out.println("Индекс элемента '" + key + "': " + index);
+        } catch (ElementNotFoundException e) {
+            System.err.println("Ошибка: " + e.getMessage());
         }
     }
 }
-
- */
