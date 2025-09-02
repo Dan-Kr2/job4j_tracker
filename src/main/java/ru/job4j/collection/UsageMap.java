8066e991ecfc7e691d26b487a6d025ea92b83218
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-09-02 18:56:37 +0300

    0. Map, HashMap [#212652 #550770]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> newMap = new HashMap<>();
         newMap.put("po4ta@gmail.com", "fio");
+        newMap.put("po4ta@yandex.ru", "fio2");
+        newMap.put("tozepo4ta@gmail.ru", "fio3");
+        newMap.put("po4ta@gmail.com", "New fio");
+
         for (String key : newMap.keySet()) {
             String value = newMap.get(key);
             System.out.println(key + " = " + value);
