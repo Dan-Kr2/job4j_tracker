205532b87a49bf0be2c9e19812b18a4c6eed90ba
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-11-03 20:28:59 +0300

    5. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -3,9 +3,14 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        return new ArrayList<>();
+        return students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 }
