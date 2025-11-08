a93f8d8245e535b47bfe910c5b747508c34c9dec
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-11-08 19:25:24 +0300

    7. Уникальность элементов и сортировка. [#110225 #554247]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -10,12 +10,14 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted(Comparator.comparing(Address::getCity))
-                .distinct()
                 .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return new ArrayList<>();
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
