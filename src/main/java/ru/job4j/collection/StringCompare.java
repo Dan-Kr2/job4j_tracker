532bab2803120d2516d250133d60a52dd28c13ae
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-11-13 19:30:09 +0300

    3. Компаратор для строк [#35008 #553337]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,8 +6,6 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
-            char a = left.charAt(i);
-            char b = right.charAt(i);
 
             if (left.charAt(i) != right.charAt(i)) {
                 return Character.compare(left.charAt(i), right.charAt(i));
