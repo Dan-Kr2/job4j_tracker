cd95278b6d4b741f17a9f35ed8837acf25265b05
Dan-Kr2 <x.real.delta@gmail.com  x.real.delta@gmail.com>
2025-12-23 23:14:39 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+import static java.lang.Math.min;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String[] a = left.split("/");
+        String[] b = right.split("/");
+        int r = min(a.length, b.length);
+        for (int i = 0; i < r; i++) {
+            int s = a[i].compareTo(b[i]);
+            if (s != 0) {
+                return -s;
+            }
+        }
+        if (a.length != b.length) {
+            return a.length - b.length;
+        }
+        return 0;
+    }
+}
